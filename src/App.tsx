6acd3a90283b852a0c51ba16fe3d4cc3184890da
winterguard <aliasbonfire@gmail.com>
2022-05-14 21:11:22 +0900

    build layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Header, Aside, Nav } from "./layouts";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App" className="App">
+      <Header />
+      <Nav />
+      <main className="main-container">11</main>
+      <Aside />
+      <footer className="footer">11111</footer>
     </div>
   );
 }
